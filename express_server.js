77b0a34007e834ab87b01b3434e1b5801938f908
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-01-27 21:39:16 +0000

    Changes made to Express

--- express_server.js
+++ express_server.js
@@ -12,6 +12,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "us@e.com",
+    password: "1234",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "us2@e.com",
+    password: "abcd",
+  },
+};
 function generateRandomString() {
   let randomString = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -21,6 +34,8 @@ function generateRandomString() {
   }
   return randomString;
 }
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -36,18 +51,22 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  // console.log(req.cookies)
+  const userId = req.cookies['user_id']
+  const user = users[userId]
+  console.log("USERS:", user)
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies['username']
+    user
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  const userId = req.cookies['user_id'] 
+  const user = users[userId]
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies['username']
+    user
   };
   
   res.render("urls_new", templateVars);
@@ -57,11 +76,12 @@ app.get("/urls/:id", (req, res) => {
   
   const id = req.params.id;
   const longURL = urlDatabase[id];
-  const username = req.cookies['username'];
+  const userId = req.cookies['user_id'];
+  const user = users[userId]
   const templateVars = { 
     id, 
     longURL,
-    username
+    user
   };
   res.render("urls_show", templateVars);
 });
@@ -77,7 +97,7 @@ app.get("/u/:id", (req, res) => {
 app.post("/urls", (req, res) => {
   
   let longURL = req.body.longURL;
-  let id = generateRandomString(); // Log the POST request body to the console
+  let id = generateRandomString(); 
   urlDatabase[id] = longURL;
   res.redirect(`/urls/${id}`);
 });
@@ -91,37 +111,77 @@ app.post("/urls/:id/delete", (req, res) => {
 app.post("/urls/:id", (req, res)=>{
   let longURL = req.body.longURL
   urlDatabase[req.params.id]= longURL
-  
   res.redirect(`/urls`)
 })
 
 app.post("/urls/:id/update", (req, res) =>{
   const updatedURL = req.body.longURL;
-  console.log(updatedURL)
   let id = req.params.id
  urlDatabase[id]= updatedURL
  res.redirect(`/urls`)
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username")
-  res.redirect('/urls')
-})
-
-
-app.post("/login", (req, res) => {
-  const username = req.body.username
-  res.cookie("username",username)
-  res.redirect('/urls')
+  res.clearCookie("user_id")
+  res.redirect('/login')
 })
 
 app.get("/login", (req,res) =>{
+  const userId = req.cookies['user_id'];
+  const user = users[userId]
   const templateVars = {
-    username: req.cookies['username']
+    user,
   }
-  res.render("_header.ejs", templateVars);
+  res.render("login", templateVars);
 })
 
+app.post("/login", (req, res) =>{
+  const email = req.body.email;
+  const password =req.body.password;
+  if(email.length === 0 || password.length === 0){
+    return res.status(400).send('error status code 400')
+  }  
+  
+  for (let id in users){
+    if (users[id].email === email && users[id].password === password){
+      res.cookie("user_id",id);
+      res.redirect("/urls");
+    }
+  }
+  res.status(403).send("invalid sign in attempt")
+console.log("REQ.USER:",users)
+
+})
+
+app.post("/register", (req, res) =>{
+  const email = req.body.email;
+  const password =req.body.password;
+  if(email.length === 0 || password.length === 0){
+   return res.status(400).send('error status code 400')
+  }
+  const id = generateRandomString();
+  thisUser={
+    id,
+    email,
+    password
+  }
+  console.log("USER:", users)
+  for (let id in users){
+    if (thisUser.email === users[id].email){
+     return res.status(400).send('error status code 400')
+    }
+  }
+  users[id]= thisUser
+
+console.log("REQ.USER:",users)
+res.cookie("user_id",id)
+res.redirect("/urls", )
+})
+
+app.get("/register", (req, res) => {
+
+  res.render("register")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
